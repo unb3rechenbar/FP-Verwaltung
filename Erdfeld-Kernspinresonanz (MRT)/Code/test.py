6806c4bb1bcdacd2499597ee907d938522db3c7b
unb3rechenbar <tom.folgmann@icloud.com>
2023-11-13 09:34:22 +0100

    Sonntagsupdate

--- Erdfeld-Kernspinresonanz (MRT)/Code/test.py
+++ Erdfeld-Kernspinresonanz (MRT)/Code/test.py
@@ -1,5 +1,8 @@
 import FPBrain as fp
 
-Wert = fp.Messwert(1,1,1)
+Wert = fp.Messwert(1,1,((1,0,0,0,0,0,0,0),(-1,0,0,0,0,0,0,0)))
+Trew = fp.Messwert(2,2,((1,0,0,0,0,0,0,0),(-1,0,0,0,0,0,0,0)))
 
-print(Wert)
+Messertergebnis = Trew + Wert
+
+print(Messertergebnis)
