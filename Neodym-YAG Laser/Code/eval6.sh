971e935df3c798c12777f6716754640aaf268b5b
unb3rechenbar <tom.folgmann@icloud.com>
2023-12-19 11:13:28 +0100

    Graphen T6

--- Neodym-YAG Laser/Code/eval6.sh
+++ Neodym-YAG Laser/Code/eval6.sh
@@ -3,19 +3,35 @@ mkdir -p "$FPNDYAG/Versuchsbericht/Bilddateien/6"
 for f in *.csv; do
     echo "Plotting $f..."
 
-    
-
-    gnuplot -e "filename='$f'; \
-            legname='datapoints'; \
-            set title '${f%.*}'; \
+    if [[ "$f" =~ "450" ]]; then
+        gnuplot -e "filename='$f'; \
+            legname='Datenpunkte'; \
             set xlabel 'Zeit [s]'; \
             set ylabel 'Spannung [V]'; \
             outputname='${f%.*}.svg'; \
             xline=1; \
             yline=3; \
+            set xrange [-0.0055:-0.002]; \
             set key right bottom; \
             a=5.000000e-06; \
             b=-6.0e-03; \
         " "$CODENDYAG/fxLinePlot.gp" 2> "${f%.*}_plotinfo.txt"
+    else
+        gnuplot -e "filename='$f'; \
+            legname='Datenpunkte'; \
+            set xlabel 'Zeit [s]'; \
+            set ylabel 'Spannung [V]'; \
+            outputname='${f%.*}.svg'; \
+            xline=1; \
+            yline=3; \
+            set xrange [-0.0035:-0.002]; \
+            set key right bottom; \
+            a=5.000000e-06; \
+            b=-6.0e-03; \
+        " "$CODENDYAG/fxLinePlot.gp" 2> "${f%.*}_plotinfo.txt"
+    fi
+
+    echo "-> Converting svg to png and moving .."
+    inkscape -w 4000 -h 2400 "${f%.*}.svg" -o "$FPNDYAG/Versuchsbericht/Bilddateien/6/${f%.*}.png"
 
 done
